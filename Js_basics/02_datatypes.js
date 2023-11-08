e2cf7ad0ea9ceb304a46f2dfd51f6ff82f4e3df1
Manish-wq <61299240+Manish-wq@users.noreply.github.com>
2023-11-08 03:05:21 +0000

    datatypes_discussed

---
+++ Js_basics/02_datatypes.js
@@ -0,0 +1,34 @@
+"use strict"; //treat all Js code as newer version
+
+//alert(3+3) we are using nodeJs not brower
+
+console.log(3
+    
+    +3) // code readability must be high.
+
+console.log('Manish') 
+
+let name = 'Manish'
+let Age = 18
+let isLoggedIn= true
+let state = '' //it's not null
+let State = null // null mtlb empty, temp aya hi ni ay.
+
+//number => 2 to 52/53
+//bigint  much larger number
+//string => ''
+//boolean => true/false
+//null => it's a standalone value/ yes datatype bhi hai
+// null ek representation hai empty value ka
+
+
+//undefined => youve not defined value yet
+// symbol => unique indentification of components.
+
+//object
+
+console.log(typeof "manish" );
+console.log(typeof Age);
+console.log(typeof null); //null is an object
+console.log(typeof undefined); //undefined hi ata hai
+
