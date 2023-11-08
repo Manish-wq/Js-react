04bf046f67a2f72707e292a8cec123322348b259
Manish-wq <61299240+Manish-wq@users.noreply.github.com>
2023-11-08 15:02:23 +0000

    added operations

--- Js_basics/03_conversionOperations.js
+++ Js_basics/03_conversionOperations.js
@@ -24,3 +24,55 @@ console.log(valueInNumber);
  console.log(stringNumber);
  console.log(typeof stringNumber);
 
+ //_______________________________________________Operations___________________________________________________________
+
+
+let value = 3
+let negValue =-value
+console.log(negValue);
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**4);
+// console.log(2/2);
+// console.log(12%5);
+
+let str1 = 'Hello '
+let str2 = 'Manish'
+let str3 = str1 + str2
+console.log(str3)
+
+
+// console.log("1" + 2)
+// console.log(1+ "2")
+// console.log("1" + 2+2)
+// console.log(2+2+"1")
+
+// console.log(3+2*2-3/4)
+
+
+console.log(+true)
+console.log("")
+
+let num1, num2, num3
+
+num1 = num2 = num3 = 2 + 5
+
+let gameCounter = 100
+// gameCounter++
+++gameCounter
+console.log(gameCounter);
+
+
+
+
+
+
+
+
+
+
+
+
+
