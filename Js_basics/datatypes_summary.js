726ae9e7435156e4496af0c24cd8c960c2fff76a
Manish-wq <61299240+Manish-wq@users.noreply.github.com>
2023-11-09 03:57:35 +0000

    studied stack n heap

--- Js_basics/datatypes_summary.js
+++ Js_basics/datatypes_summary.js
@@ -43,6 +43,31 @@ console.log(typeof myfunc);
 console.log(typeof anotherId);
 
 
+//_____________________Game of memory_ and_threads________________________
+
+// There are two types of memory
+// Stack(Primitive), Heap(Non-Primitive)
+
+let myYoutubename = 'manishfuturist'
+ 
+let anothername = myYoutubename
+anothername = 'chaiaurcode'
+
+console.log(myYoutubename);
+console.log(anothername);
+
+let userOne = {
+    email: 'user@google.com',
+    upi: 'user@paytm'
+
+}
+let userTwo = userOne
+
+userTwo.email = 'manish@google.com'
+
+console.log(userOne.email)
+console.log(userTwo.email)
+
 
 
 
